96486a2e7a2272f2b78b0371cb53c27bea94bce4
Passam <119688260+Passam02@users.noreply.github.com>
2023-01-27 02:00:04 +0100

    Add show names under images

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const form = document.querySelector('#searchForm')
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    console.dir(form)
-    console.log(form[0].form.elements[0].value)
-    for (var i = document.images.length; i-->0;)
-    document.images[i].parentNode.removeChild(document.images[i]);
+    //for (var i = document.images.length; i-->0;)
+    //document.images[i].parentNode.removeChild(document.images[i]);
+    const divs = document.querySelectorAll('.shows');
+    divs.forEach(box => {
+    box.remove();
+});
     const searchInput = form[0].form.elements[0].value
     const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchInput}`)
-    console.log(response.data)
     makeImages(response.data)
 })
 
 const makeImages = (tvShows) => {
     for (let show of tvShows) {
         if (show.show.image) {
-            console.log(show)
+            const div = document.createElement('DIV')
             const img = document.createElement('IMG')
+            const name = document.createElement('h4')
+            name.innerText = show.show.name
             img.src = show.show.image.medium
-            document.body.append(img)
+            document.body.append(div)
+            div.append(img)
+            div.append(name)
+            div.classList.add('shows')
         }
     }
 }
